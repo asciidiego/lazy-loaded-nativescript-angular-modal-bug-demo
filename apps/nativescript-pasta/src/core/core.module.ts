7f446123f0d02e202c5eb91c7fa60bde4ccceb46
Diego Rodriguez <diego.rodriguez@polaroo.com>
2020-06-20 20:55:27 +0200

    Revert "feature(pasta): generate app"
    This reverts commit 2d58b99bc9be5b4069d45109377f7a9d6fe458f3.

--- apps/nativescript-pasta/src/core/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-// libs
-import { TimeCoreModule } from '@time/nativescript';
-
-@NgModule({
-  imports: [TimeCoreModule]
-})
-export class CoreModule {}
--- xplat/nativescript/core/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './services';
-export {TimeCoreModule} from './core.module';
